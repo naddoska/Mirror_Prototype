b2e7ffe733e9dba65de98fdf65b8e6cb989b946e
Nadine Petzka <nadine.petzka-hk@uni-wuppertal.de>
2022-12-06 15:15:37 +0100

    Avatar movement + Camera switch

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Mirror; 
+
+public class FollowPlayer : NetworkBehaviour
+{
+    public GameObject Camera;
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isLocalPlayer)
+            CmdMove();
+    }
+
+    private void CmdMove()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
+
+    
+
+
--- Assets/Scripts/Player1_Controller.cs
+++ Assets/Scripts/Player1_Controller.cs
@@ -1,4 +1,6 @@
 using Mirror;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -6,23 +8,63 @@ public class Player1_Controller : NetworkBehaviour
 {
     public Rigidbody Player1;
     public float horizontalInput;
+    public float rotationInput;
     public float verticalInput;
     public float speed = 10.0f;
- 
+    public float height = 1.8f;
+    private bool isfirstperson = true; 
+
     // Update is called once per frame
-    void FixedUpdate()
+    public override void OnStartLocalPlayer()
     {
-        if(isLocalPlayer) 
-        CmdMove();
+        Camera.main.transform.SetParent(transform);
+        Camera.main.transform.localPosition = new Vector3( 0, height, 0);
+        Camera.main.transform.localRotation = Quaternion.identity;
     }
+
+    private void Update()
+    {
+        if (isLocalPlayer)
+        {
+            CameraSwitch();
+            CmdMove();
+        }
+    }
+
+
 
     private void CmdMove()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        //if (Input.GetMouseButtonDown(0)) mousemovement for later
+        //{
+            //rotationInput = Input.
+        //}
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        // transform.Rotate
     }
+
+    private void CameraSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (isfirstperson)
+            {
+                Camera.main.transform.localPosition = new Vector3(0, 2, -1);
+                Debug.Log("Aussen");
+                isfirstperson = false;
+            }
+            else
+            {
+                Camera.main.transform.localPosition = new Vector3(0, height, 0);
+                Debug.Log("Innen");
+                isfirstperson = true;
+            }
+        }
+    }
+
 
     
 }
