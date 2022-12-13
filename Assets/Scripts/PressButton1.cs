3e5d9de6154b27de3559a3f60d2727af82697018
Nadine Petzka <nadine.petzka-hk@uni-wuppertal.de>
2022-12-13 13:56:48 +0100

    Button added

--- Assets/Scripts/PressButton1.cs
+++ Assets/Scripts/PressButton1.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
 
 public class PressButton1 : MonoBehaviour
 {
+    public Button Button1; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button1.onClick.AddListener(WeristColab);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void WeristColab()
+    {
+        Application.OpenURL("https://fk9.uni-wuppertal.de/de/studium/projekt-colab/");
     }
 }
